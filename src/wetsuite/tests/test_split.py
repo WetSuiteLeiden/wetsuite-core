db3cb416b2f37692668619c07b815da25fc4cc3f
scarfboy <scarfboy@gmail.com>
2024-05-20 18:18:38 +0200

    more tests for the splitter

--- src/wetsuite/tests/test_split.py
+++ src/wetsuite/tests/test_split.py
@@ -5,6 +5,7 @@ import wetsuite.helpers.split
 
 
 def test_ascii_fix():
+    ' test that this dumb fixing function does a thing, and does not break itself '
     assert b'UTF' in wetsuite.helpers.split.fix_ascii_blah( b'   <?xml version="1.0" encoding="US-ASCII"?>  <a/>' )
 
 def test_decide():
@@ -15,7 +16,26 @@ def test_decide():
         'bwb_toestand.xml',
         'cvdr_example1.xml',
         'eggs.pdf',
+        'gmb.xml',
+        'prb.xml',
     ):
         bwb_example_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
         with open(bwb_example_path,'rb') as f:
-            wetsuite.helpers.split.decide( f.read() )
+            for _, _ in wetsuite.helpers.split.decide( f.read() ):
+                pass
+
+def test_fragments():
+    ' see whether decide() deals with a file'
+    import test_split
+
+    for test_path in ( # these are test files in the repo
+        'bwb_toestand.xml',
+        'cvdr_example1.xml',
+        'eggs.pdf',
+        'gmb.xml',
+        'prb.xml',
+    ):
+        bwb_example_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
+        with open(bwb_example_path,'rb') as f:
+            for _, procobj in wetsuite.helpers.split.decide( f.read() ):
+                list( procobj.fragments() )
