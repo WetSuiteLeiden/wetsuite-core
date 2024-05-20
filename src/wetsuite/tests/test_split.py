5331cd0cbbf3feaf62c403704fa3f761bd4e19da
scarfboy <scarfboy@gmail.com>
2024-05-21 00:02:44 +0200

    improving test coverage

--- src/wetsuite/tests/test_split.py
+++ src/wetsuite/tests/test_split.py
@@ -1,7 +1,11 @@
 ''' test functions in the wetsuite.helpers.split module '''
 
 import os
+
+import pytest
+
 import wetsuite.helpers.split
+import wetsuite.helpers.etree
 
 
 def test_ascii_fix():
@@ -19,8 +23,8 @@ def test_decide():
         'gmb.xml',
         'prb.xml',
     ):
-        bwb_example_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
-        with open(bwb_example_path,'rb') as f:
+        one_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
+        with open(one_path,'rb') as f:
             for _, _ in wetsuite.helpers.split.decide( f.read() ):
                 pass
 
@@ -33,9 +37,64 @@ def test_fragments():
         'cvdr_example1.xml',
         'eggs.pdf',
         'gmb.xml',
+        'gmb.html',
         'prb.xml',
     ):
-        bwb_example_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
-        with open(bwb_example_path,'rb') as f:
+        one_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
+        with open(one_path,'rb') as f:
             for _, procobj in wetsuite.helpers.split.decide( f.read() ):
                 list( procobj.fragments() )
+
+
+def test_firstonly():
+    ' see whether decide() deals with a file'
+    import test_split
+
+    gmb_path = os.path.join( os.path.dirname( test_split.__file__ ), 'gmb.xml' )
+    with open(gmb_path,'rb') as f:
+        docbytes = f.read()
+
+    assert (
+        len( wetsuite.helpers.split.decide( docbytes ) ) > 1   # ther are more than one (we need an example for which that is true)
+        and
+        len( wetsuite.helpers.split.decide( docbytes, first_only=True ) ) == 1 # but not if you ask for one
+    )
+
+
+def test__split_op_xml__start_at_none():
+    ' test the code path for starting at root '
+    import test_split
+    gmb_path = os.path.join( os.path.dirname( test_split.__file__ ), 'gmb.xml' )
+    with open( gmb_path, 'rb') as gmb_file:
+        gmb_tree = wetsuite.helpers.etree.fromstring( gmb_file.read() )
+        wetsuite.helpers.split._split_op_xml( gmb_tree, start_at=None)
+
+
+def test__split_op_xml__start_at_nonsemse():
+    ' test the code path for starting at root '
+    import test_split
+    gmb_path = os.path.join( os.path.dirname( test_split.__file__ ), 'gmb.xml' )
+    with open( gmb_path, 'rb') as gmb_file:
+        gmb_tree = wetsuite.helpers.etree.fromstring( gmb_file.read() )
+        with pytest.raises(ValueError,  match=r'.*Did not find.*'):
+            wetsuite.helpers.split._split_op_xml( gmb_tree, start_at='/przewalski')
+
+
+
+def test_Fragments_nonbytes():
+    ' test that it complains about input that is not bytes '
+    with pytest.raises(ValueError,  match=r'.*bytestrings.*'):
+        wetsuite.helpers.split.Fragments('')
+
+
+def test_Fragments_notimplemented():
+    ' test that it complains when not implemented (arguably only valuable to test on all implementation classes in the same module)'
+    fop = wetsuite.helpers.split.Fragments(b'')
+    with pytest.raises(NotImplementedError,  match=r'.*Please.*'):
+        fop.accepts()
+
+    with pytest.raises(NotImplementedError,  match=r'.*Please.*'):
+        fop.suitableness()
+
+    with pytest.raises(NotImplementedError,  match=r'.*Please.*'):
+        fop.fragments()
