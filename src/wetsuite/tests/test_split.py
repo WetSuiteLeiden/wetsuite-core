2350548e75d7771ab0b80f3a43647a0baceba074
scarfboy <scarfboy@gmail.com>
2024-05-21 22:08:01 +0200

    generalize test code a little

--- src/wetsuite/tests/test_split.py
+++ src/wetsuite/tests/test_split.py
@@ -7,6 +7,17 @@ import pytest
 import wetsuite.helpers.split
 import wetsuite.helpers.etree
 
+test_file_list = ( # these are test files in the repo
+    'bwb_toestand.xml',
+    'cvdr_example1.xml',
+    'eggs.pdf',
+    'gmb.xml',
+    'gmb.html',
+    'gmb.html.zip',
+    'prb.xml',
+    'bgr.html',
+    'rechtspraak.xml',
+)
 
 def test_ascii_fix():
     ' test that this dumb fixing function does a thing, and does not break itself '
@@ -16,13 +27,7 @@ def test_decide():
     ' see whether decide() deals with a file'
     import test_split
 
-    for test_path in ( # these are test files in the repo
-        'bwb_toestand.xml',
-        'cvdr_example1.xml',
-        'eggs.pdf',
-        'gmb.xml',
-        'prb.xml',
-    ):
+    for test_path in test_file_list:
         one_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
         with open(one_path,'rb') as f:
             for _, _ in wetsuite.helpers.split.decide( f.read() ):
@@ -32,14 +37,7 @@ def test_fragments():
     ' see whether decide() deals with a file'
     import test_split
 
-    for test_path in ( # these are test files in the repo
-        'bwb_toestand.xml',
-        'cvdr_example1.xml',
-        'eggs.pdf',
-        'gmb.xml',
-        'gmb.html',
-        'prb.xml',
-    ):
+    for test_path in test_file_list:
         one_path = os.path.join( os.path.dirname( test_split.__file__ ), test_path )
         with open(one_path,'rb') as f:
             for _, procobj in wetsuite.helpers.split.decide( f.read() ):
