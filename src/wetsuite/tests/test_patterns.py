d24cffc4a17ed82f79dac658f6e0f4cefded1801
scarfboy <scarfboy@gmail.com>
2024-09-08 23:07:34 +0200

    moved test_abbrevs into test_patterns, added tests for better coverage

--- src/wetsuite/tests/test_patterns.py
+++ src/wetsuite/tests/test_patterns.py
@@ -1,7 +1,7 @@
 """ Tests relating to the patterns module
 """
 
-from wetsuite.helpers.patterns import find_references
+from wetsuite.helpers.patterns import find_references, mark_references_spacy, abbrev_find, abbrev_count_results
 
 
 
@@ -122,3 +122,75 @@ def test_found_artikel():
 
     ]:
         assert len( find_references( test ) ) > 0   # found anything at all?
+
+
+def test_mark_up_spacy():
+    import spacy
+    nlp = spacy.blank('nl')
+    doc = nlp.make_doc("Nee, artikel 3; vla")
+    mark_references_spacy(doc)
+    assert doc.ents[0].text == 'artikel 3'
+
+
+" test the abbreviation part of the pattern helper module "
+import wetsuite.helpers.patterns
+
+
+def test_form1():
+    "Test this variant of abbreviation"
+    assert wetsuite.helpers.patterns.abbrev_find("A Bree Veation (ABV)") == [
+        ("ABV", ["A", "Bree", "Veation"])
+    ]
+
+
+def test_form2():
+    "Test this variant of abbreviation"
+    assert wetsuite.helpers.patterns.abbrev_find("(ABV) A Bree Veation") == [
+        ("ABV", ["A", "Bree", "Veation"])
+    ]
+
+
+def test_form3():
+    "Test this variant of abbreviation"
+    assert wetsuite.helpers.patterns.abbrev_find("ABV (A Bree Veation)") == [
+        ("ABV", ["A", "Bree", "Veation"])
+    ]
+
+
+def test_form4():
+    "Test this variant of abbreviation"
+    assert wetsuite.helpers.patterns.abbrev_find("(A Bree Veation) ABV") == [
+        ("ABV", ["A", "Bree", "Veation"])
+    ]
+
+
+def test_abbrev_count_results():
+    "counting of extracted abbreviations"
+    parts = [
+        wetsuite.helpers.patterns.abbrev_find(
+            "A Bree Veation (ABV) and (ABV) A Bree Veation"
+        ),
+        wetsuite.helpers.patterns.abbrev_find(
+            "ABV (A Bree Veation) and (A Bree Veation) ABV"
+        ),
+    ]
+    counts = wetsuite.helpers.patterns.abbrev_count_results(
+        parts
+    )  # counts how many documents contain it
+    assert counts == {"ABV": {("A", "Bree", "Veation"): 2}}
+
+
+
+def test_abbrev_count_results_case_insensitive():
+    "Tests that case_insensitive_explanations merges counts between same-but-for-capitalisation variants, and chooses the most common capitalisation"
+    results = wetsuite.helpers.patterns.abbrev_count_results(
+        # This signifies three documents with a very similar abbreviation
+        (
+            [('ABV', ['A', 'Bree', 'Veation']),
+             ('SE',  ['Something', 'Else'])],
+            [('ABV', ['A', 'bree', 'veation'])],
+            [('ABV', ['A', 'bree', 'veation'])],
+        ),
+    case_insensitive_explanations=True
+    )
+    assert results['ABV'] == {('A', 'bree', 'veation'): 3}
