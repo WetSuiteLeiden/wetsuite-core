f73729fdf7068eaeb4886cda0052590ea3dda1bd
scarfboy <scarfboy@gmail.com>
2024-11-15 16:52:34 +0100

    covering more code in test

--- src/wetsuite/tests/test_koop_frbr.py
+++ src/wetsuite/tests/test_koop_frbr.py
@@ -7,8 +7,9 @@ import wetsuite.helpers.localdata
 import wetsuite.datacollect.koop_frbr
 
 
+
 def test_koop_frbr_init():
-    "test that that class even instatiates"
+    " test that that class even instatiates "
     fetch_store = wetsuite.helpers.localdata.LocalKV(":memory:", str, bytes)
     cache_store = wetsuite.helpers.localdata.LocalKV(":memory:", str, bytes)
 
@@ -16,4 +17,20 @@ def test_koop_frbr_init():
         fetch_store=fetch_store, cache_store=cache_store
     )
 
-    # ff.handle_url('https://repository.overheid.nl/frbr/officielepublicaties/ah-tk/19961997/ah-tk-19961997-100/1/html')
+
+def DISABLED_test_koop_frbr_fetch():
+    """ test of code involved in fetching.  
+        By actually fetching a document set, so this takes at least a dozen seconds, which is why this is disabled for now. """
+    fetch_store = wetsuite.helpers.localdata.LocalKV(":memory:", str, bytes)
+    cache_store = wetsuite.helpers.localdata.LocalKV(":memory:", str, bytes)
+
+    ff = wetsuite.datacollect.koop_frbr.FRBRFetcher(
+        fetch_store=fetch_store, cache_store=cache_store
+    )
+
+    ff.add_page('https://repository.overheid.nl/frbr/officielepublicaties/bgr/2015/bgr-2015-1/1')
+    list( ff.work() )
+
+    # IIRC three each
+    assert len(fetch_store) > 0
+    assert len(cache_store) > 0
