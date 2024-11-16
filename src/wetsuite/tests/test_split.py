570af6e6cdbe8b71c435680d6901260899334bf1
scarfboy <scarfboy@gmail.com>
2024-11-16 14:38:17 +0100

    adding some profiling to splitting (on its tests, for no particular reason)

--- src/wetsuite/tests/test_split.py
+++ src/wetsuite/tests/test_split.py
@@ -1,13 +1,13 @@
 """ test functions in the wetsuite.helpers.split module """
 
-import os
+import os, time
 
 import pytest
 
 import wetsuite.helpers.split
 import wetsuite.helpers.etree
 
-test_file_list = (  # these are test files also in the repo
+_test_file_list = (  # these are test files also in the repo
     "bwb_toestand.xml",
     "cvdr_example1.xml",
     "gmb.html",
@@ -40,12 +40,14 @@ def test_decide():
     "see whether decide() deals with each test file without failing"
     import test_split
 
-    for test_path in test_file_list:
+    for test_path in _test_file_list:
         one_path = os.path.join(
             os.path.dirname(test_split.__file__), "testfiles", test_path
         )
         with open(one_path, "rb") as f:
-            for _, _ in wetsuite.helpers.split.decide(f.read(), debug=True):
+            filedata = f.read()
+
+            for _, _ in wetsuite.helpers.split.decide(filedata, debug=True):
                 pass
 
 
@@ -53,12 +55,13 @@ def test_fragments():
     "see whether fragments() deals with each test file without failing"
     import test_split
 
-    for test_path in test_file_list:
+    for test_path in _test_file_list:
         one_path = os.path.join(
             os.path.dirname(test_split.__file__), "testfiles", test_path
         )
         with open(one_path, "rb") as f:
-            for _, procobj in wetsuite.helpers.split.decide(f.read()):
+            filedata = f.read()
+            for _, procobj in wetsuite.helpers.split.decide(filedata):
                 list(procobj.fragments())
 
 
@@ -96,7 +99,7 @@ def test__split_officielepublicaties_xml__list_test():
         )  # pylint: disable=protected-access
 
 
-def test__split_officielepublicaties_xml__start_at_nonsemse():
+def test__split_officielepublicaties_xml__start_at_nonsense():
     "test the code path for starting at path that does not exist"
     import test_split
 
@@ -143,3 +146,37 @@ def test_Fragments_notimplemented():
 
     with pytest.raises(NotImplementedError, match=r".*Please.*"):
         fop.fragments()
+
+
+
+
+if __name__ == '__main__':
+    # When run as a main script this profiles (primarily) test_decide and test_fragments 
+    #  because as o this writing they take ~2s each (more when profiled), as each file takes 0.4s to decide
+    from cProfile import Profile
+    from pstats import SortKey, Stats
+
+    for func in (
+        test__split_officielepublicaties_xml__list_test,
+        test__split_officielepublicaties_xml__start_at_node,
+        test__split_officielepublicaties_xml__start_at_none,
+        test__split_officielepublicaties_xml__start_at_nonsense,
+        test_ascii_fix,
+        test_decide,
+        test_fragments,
+        test_Fragments_nonbytes,
+        test_Fragments_notimplemented,
+    ):
+        start = time.time()
+        func()
+        took = time.time() - start
+        if took > 0.2:
+            print(f" ====== {func.__name__} took {took:.2f}sec, profiling... ===== ")
+            with Profile() as profile:
+                print(f"{func()} = ")
+                (
+                    Stats(profile)
+                    #.strip_dirs()
+                    .sort_stats(SortKey.CUMULATIVE)
+                    .print_stats(40)
+                )
