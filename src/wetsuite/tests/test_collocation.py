a4cfe467f4325d53ec3df6fa1658f9b1ef009d7a
scarfboy <scarfboy@gmail.com>
2025-04-06 19:32:43 +0200

    adding basic tests for collocation module

--- src/wetsuite/tests/test_collocation.py
+++ src/wetsuite/tests/test_collocation.py
@@ -1,4 +1,56 @@
 """ test functions in the wetsuite.helpers.collocation module """
 
-# import os
-# import wetsuite.helpers.collocation
+import os
+import wetsuite.helpers.collocation
+import wetsuite.helpers.etree
+
+
+
+
+def test_text():
+    ' get text from one of our own test files.  Test values below were chosen based on its contents '
+    import test_collocation  # that's intentional pylint: disable=W0406
+    with open( os.path.join(os.path.dirname(test_collocation.__file__), "testfiles", 'bwb_toestand.xml'), mode="rb" ) as f:
+        tree = wetsuite.helpers.etree.fromstring( f.read() )
+        text = wetsuite.helpers.etree.html_text( tree )
+        return text
+
+
+def test_basics():
+        text = test_text()
+
+        coll = wetsuite.helpers.collocation.Collocation()
+        coll.consume_tokens( text.split(), gramlens=(3,) )
+        coll.cleanup_ngrams(   mincount=10 )
+        coll.cleanup_unigrams( mincount=2  )
+
+        grams = coll.score_ngrams()
+        def has_sequence(test_tup):
+            for tup, _, _, _ in grams:
+                if test_tup == tup:
+                    return True
+            return False
+
+        assert has_sequence( ('als','bedoeld','in') )
+        assert has_sequence( ('tot', 'geheimhouding', 'verplicht') )
+        assert not has_sequence( ('foo', 'bar', 'quu') )
+
+
+
+def test_cleanup():
+    text = test_text()
+
+    coll = wetsuite.helpers.collocation.Collocation()
+    coll.consume_tokens( text.split(), gramlens=(3,) )
+
+    assert coll.counts()['ngrams'] > 1000
+    coll.cleanup_ngrams( mincount=10 )
+    assert coll.counts()['ngrams'] < 100
+
+    assert coll.counts()['unigrams'] > 1000
+    coll.cleanup_unigrams( mincount=10 )
+    assert coll.counts()['unigrams'] < 200
+
+
+
+
