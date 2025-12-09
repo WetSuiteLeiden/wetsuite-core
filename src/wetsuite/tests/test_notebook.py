0c175ca934d9d7d6bf4b373028486967e8f8de54
scarfboy <scarfboy@gmail.com>
2025-12-09 16:16:27 +0100

    added some monkey patching to test import fallbacks

--- src/wetsuite/tests/test_notebook.py
+++ src/wetsuite/tests/test_notebook.py
@@ -1,5 +1,6 @@
 " tests related to ipyton/jupyter notebook code "
 import time
+import pytest
 
 import wetsuite.helpers.etree
 from wetsuite.helpers.notebook import (
@@ -7,14 +8,15 @@ from wetsuite.helpers.notebook import (
     is_interactive,
     is_ipython,
     is_notebook,
+    is_ipython_interactive,
     progress_bar,
     ProgressBar,
     etree_visualize_selection,
 )
 
 
-def test_count_normalized():
-    "test that detect_env, well, detects the pyenv special case"
+def test_detect_env_pytest():
+    "test that detect_env, well, detects the pytest special case"
     d = detect_env()
     # from within pytest it's probably...
     assert d["ipython"] is False
@@ -24,10 +26,66 @@ def test_count_normalized():
     assert not is_ipython()
     assert not is_interactive()
     assert not is_notebook()
+    assert not is_ipython_interactive()
+
+
+def test_detect_env_colab(monkeypatch):
+    ' Fake that we have google colab '
+    import sys, types
+    
+    with pytest.raises(ImportError):
+        import google.colab # we shouldn't have it already
+
+    # knowing that detect_env treats exactly this as a special case, make it skip that
+    monkeypatch.delitem( sys.modules, "pytest", raising=False)
+    
+    # Makes 'import google.colab' work - this is awkward and probably not right or minimal, but good enough for the test.
+    go = types.ModuleType('google')
+    go.__path__ = []
+    co = types.ModuleType("google.colab")
+    go.colab = co
+    monkeypatch.setitem( sys.modules, 'google',       go )
+    monkeypatch.setitem( sys.modules, 'google.colab', co )
+
+    d = detect_env()
+    assert d.get("colab",None) is True
+    assert d["ipython"] is True
+    assert d["interactive"] is True
+    assert d["notebook"] is True
+
+    assert is_ipython()
+    assert is_interactive()
+    assert is_notebook()
+    assert is_ipython_interactive()
+
+
+#a test_detect_env_ipython would probabably be too much mockery. At this point we'd be doing it only for the test coverage
+
 
 
 def test_progress_console():
-    "test that it does not bork out"
+    "test that it does not bork out trying to make a progess bar (probably defaulting to tqdm console, due to pytest environment) "
+    pb = progress_bar(10)
+    pb.value += 1
+    pb.description = "foo"
+
+
+def test_progress_nbfallback(monkeypatch):
+    " Pretend tqdm isn't installed, see if it falls back"
+
+    ## monkey patching to pretend something cannot be loaded. 
+    import builtins
+    real_import = builtins.__import__
+    def filtering_import(name, *args, **kwargs):
+        if name == "tqdm":
+            raise ModuleNotFoundError(name)
+        return real_import(name, *args, **kwargs)
+    monkeypatch.setattr(builtins, "__import__", filtering_import)
+
+    # test that the above monkey patching actually blocks the import
+    with pytest.raises(ImportError):
+        import tqdm
+
     pb = progress_bar(10)
     pb.value += 1
     pb.description = "foo"
